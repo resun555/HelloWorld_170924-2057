485d8b918f72bcbf6db1badc34c66e52001b462c
Sam Iris <resun555@live.cn>
2017-09-24 20:53:09 +0800

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+int main()
+{
+	printf("HelloWorld!");
+    return 0;
+}
+
